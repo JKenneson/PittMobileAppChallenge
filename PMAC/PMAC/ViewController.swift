6471a06f6c56f0324fe8c5f5c6214660fc013c41
Jonathan Kenneson <jon@Jonathans-iMac.local>
2017-02-17 21:00:35 -0500

    Added buttons to track the user's position and the entire route.  User position implemented

--- PMAC/PMAC/ViewController.swift
+++ PMAC/PMAC/ViewController.swift
@@ -13,11 +13,16 @@ import CoreLocation
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     //MARK: IBOutlet Declarations
-    @IBOutlet weak var MapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var entireRouteButton: UIButton!
+    @IBOutlet weak var currentPositionButton: UIButton!
     
     //MARK: Global variable declarations
     let locationManager = CLLocationManager()
+    var firstLocation: CLLocation? //To track our initial location
     var isFirstLoad = true
+    var isTrackingPosition = true     //Default to tracking the position only
+    var isTrackingRoute = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +32,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
 
-        self.MapView.showsUserLocation = true
-        
-        isFirstLoad = true
+        self.mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,10 +41,34 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     
+    //MARK: Position and Route Button Methods
+    
+    
+    /// Will switch the map view to tracking the entire route in the map region
+    ///
+    /// - Parameter sender: The Entire Route UIButton
+    @IBAction func trackEntireRouteButton(_ sender: UIButton) {
+        print("Entire Route")
+        self.isTrackingRoute = true
+        self.isTrackingPosition = false
+    }
+    
+    
+    /// Will switch the map view to tracking only the user in the map region
+    ///
+    /// - Parameter sender: The Current Position UIButton
+    @IBAction func trackCurrentPositionButton(_ sender: UIButton) {
+        print("Tracking Position")
+        self.isTrackingPosition = true
+        self.isTrackingRoute = false
+    }
+    
+    //MARK: Map View Methods
+    
+    
     
     
     //MARK: Location Delegate Methods
-    
     
     
     /// Called every time a location gets updated in the location manager, the locations are
@@ -54,11 +81,19 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         let location = locations.last
         
-        if (isFirstLoad) {     //First time through the loop, zoom to the user's location
-            initialZoomToPosition(locationToZoom: location!)
+        if(self.isFirstLoad) {               //Save the initial location
+            self.firstLoad(initialLocation: location!)
         }
+        else if (self.isTrackingPosition) {    //Follow the user by setting the map region to their current position
+            self.trackPosition(locationToZoom: location!)
+        }
+        else if (self.isTrackingRoute) {       //Track the user's route by setting the map region to the entire span of the route
         
-        print (locations.count)
+        }
+        else {                          //Allow the user to move around the map
+            //For now, do nothing
+
+        }
         
         //Option to stop updating location below
         //self.locationManager.stopUpdatingLocation()
@@ -66,22 +101,44 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     
     
-    /// Initially, the map will zoom into the location of the user, 
-    /// this can be animated or non-animated by changing the animate parameter to true/false respectively
+    /// The map will track the user's position and keep them in the center of the map screen
     ///
     /// - Parameter locationToZoom: A CLLocation that will be the center of the map screen when we zoom in
-    func initialZoomToPosition(locationToZoom: CLLocation)  {
+    func trackPosition(locationToZoom: CLLocation)  {
         let center = CLLocationCoordinate2D(latitude: locationToZoom.coordinate.latitude, longitude: locationToZoom.coordinate.longitude)   //Find the center
         
         var region = MKCoordinateRegion()
         
         //Zoom into the location of the user with a radius of 1
         region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        self.MapView.setRegion(region, animated: false)  //Change whether or not to be animated
-        
-        isFirstLoad = false
+        self.mapView.setRegion(region, animated: true)  //Change whether or not to be animated
     }
     
+    /// The map will track the user's entire route and keep it in the center of the map screen
+    ///
+    func trackRoute()  {
+        //Not yet implemented
+        
+    }
+    
+    
+    /// Initially, the map will locate the user and save their starting point as firstLocation
+    ///
+    /// - Parameter locationToZoom: A CLLocation that will be the center of the map screen when we zoom in
+    func firstLoad(initialLocation: CLLocation)  {
+        
+        self.firstLocation = initialLocation
+        
+        let center = CLLocationCoordinate2D(latitude: locationToZoom.coordinate.latitude, longitude: locationToZoom.coordinate.longitude)   //Find the center
+        var region = MKCoordinateRegion()
+        
+        //Zoom into the location of the user with a radius of .01
+        region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        self.mapView.setRegion(region, animated: false)  //Change whether or not to be animated
+        
+        self.isFirstLoad = false
+    
+    }
     
     
     /// Handling the errors from the location manager
