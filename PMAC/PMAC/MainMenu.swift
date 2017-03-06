bd71a4f380f9d66d4f4547e06cb2f06063bfd8f0
Jonathan Kenneson <jmk213@pitt.edu>
2017-03-06 16:34:39 -0500

    Helper method added to return the correct stage image on the main screen

--- PMAC/PMAC/MainMenu.swift
+++ PMAC/PMAC/MainMenu.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class MainMenu: UIViewController {
     
+    //MARK: IBOutlets
     @IBOutlet weak var newRunButton: UIButton!
     @IBOutlet weak var treeStatsButton: UIButton!
     @IBOutlet weak var mainMenuImageView: UIImageView!
+    
+    //MARK: Global variables
+    var stageCounter = 0        //Used for testing
     
     //Called when the view loads
     override func viewDidLoad() {
@@ -32,6 +36,39 @@ class MainMenu: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    @IBAction func treeStatsButtonPushed(_ sender: Any) {
+        self.stageCounter += 1
+        mainMenuImageView.image = getStage(stageNumber: self.stageCounter % 5 + 1)
+    }
+    
+    
+    
+    //MARK: ImageVIew Helper Methods
+    
+    
+    /// Returns the main menu stage from the pased in number
+    ///
+    /// - Parameter stageNumber: Which stage to return
+    /// - Returns: The UIImage to draw on the main menu
+    func getStage(stageNumber: Int) -> UIImage {
+        switch stageNumber {
+        case 1:
+            return UIImage(named: "1st stage")!
+        case 2:
+            return UIImage(named: "2nd stage")!
+        case 3:
+            return UIImage(named: "3rd stage")!
+        case 4:
+            return UIImage(named: "4th stage")!
+        case 5:
+            return UIImage(named: "5th stage")!
+        //Return the first stage in the default case
+        default:
+            return UIImage(named: "1st stage")!
+        }
+    }
     
     
 }
