8c023d15f07e8e479dc58e80d30943d3df5b3835
Jonathan Kenneson <jmk213@pitt.edu>
2017-03-17 13:24:35 -0400

    Formatting Start Screen across devices

--- PMAC/PMAC/StartScreen.swift
+++ PMAC/PMAC/StartScreen.swift
@@ -14,6 +14,7 @@ class StartScreen: UIViewController {
     
     
     @IBOutlet weak var startScreenBackground: UIImageView!
+    @IBOutlet weak var riseButton: UIButton!
     
     //Called when the view loads
     override func viewDidLoad() {
@@ -22,6 +23,7 @@ class StartScreen: UIViewController {
         //Draw the background image based on the current stage
         self.startScreenBackground.image = getStageImage(stageNumber: Globals.treeStage)
         
+        self.riseButton.layer.cornerRadius = 4  //Bevel that button
         
     }
     
